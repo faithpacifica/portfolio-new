9e8b2f6d541defbffa43c4e78d543d33515e0637
Dildora Muslimova <faithpacifica@yahoo.com>
2026-03-04 23:36:47 +0900

    setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
-
 function App() {
+  return <>
+  <h1 className="text-blue-700 text-7xl">
 
-  return (
-    <>
-    PORTFOLIO
-    </>
-  )
+  PORTFOLIO
+  </h1>
+  </>;
 }
 
-export default App
+export default App;
